bf34deba869a30f30bd101da03e575027b9bc5af
Théo MARECHAL <theo.marechal0@gmail.com>
2023-01-19 23:50:18 +0100

    Add time in DebugPrint & Update ReadMe

--- API/API/index.js
+++ API/API/index.js
@@ -1,5 +1,7 @@
 const express = require('express') // Required for the REST API to work.
-const app = express() // Create the REST API.
+
+const app = express() // Create the REST API
+//Middlewareapp.use(express.json())
 const LISTENING_PORT = 8080
 const DEBUG = true
 let ERROR_CODES = new Object()
@@ -9,12 +11,28 @@ ERROR_CODES.AccountUnauthorised = -3
 ERROR_CODES.PositionUnavailable = -4
 
 
+function GetDate() {
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+    return (date + "-" + month + "-" + year);
 
+}
+
+function GetTime() {
+    let date_ob = new Date();
+    let hours = date_ob.getHours();
+    let minutes = date_ob.getMinutes();
+    let seconds = date_ob.getSeconds();
+    return (hours+":"+minutes+":"+seconds)
+
+}
 
 function DebugPrint(data) {
     if(DEBUG == true)
     {
-        console.log(data)
+        console.log("["+GetDate()+"] ["+GetTime()+"] -> "+data)
     }
 }
 
@@ -29,7 +47,7 @@ function HandlePositionHistoryRequest(req,res) {
     let ToReturn = new Object() // Create the return json object.
     const id = parseInt(req.params.id) // Get the id passed in parameters.
     DebugPrint("Received position history request for "+id.toString()+".")
-    ToReturn.error = GetErrorJson() // Storing the ErrorJson object template in the ToRetuen json object.
+    ToReturn.error = GetErrorJson() // Storing the ErrorJson object template in the ToReturn json object.
     ToReturn.history = [] // Make an array to store all the position retrieved from the DB.
     ToReturn.history[0] = new Object()
     ToReturn.history[0].posx = 9.56454654 // Setting PosX of the position in the object.
@@ -42,16 +60,25 @@ function HandlePositionActualRequest(req,res) {
     let ToReturn = new Object() // Create the return json object.
     const id = parseInt(req.params.id) // Get the id passed in parameters.
     DebugPrint("Received actual position request for "+id.toString()+".")
-    ToReturn.error = ErrorJson // Storing the ErrorJson ocject template in the ToRetuen json object.
+    ToReturn.error = GetErrorJson() // Storing the ErrorJson ocject template in the ToReturn json object.
     ToReturn.now = new Object() // Creating the json now object. 
     ToReturn.now.posx = 9.56454654 // Setting PosX of the tracker in the object.
     ToReturn.now.posy = 15.56454654 // Setting PosY of the tracker in the object.
     res.status(200).json(ToReturn) // Reply with the json object.
 }
 
+function HandleUserInfoRequest(req,res) {
+    let ToReturn = new Object() // Create the return json object.
+    DebugPrint("Received user information request for "+req.params.mail+"with password : "+req.params.password+".")
+    ToReturn.error = GetErrorJson() // Storing the ErrorJson ocject template in the ToReturn json object.
+    ToReturn.session_id = 0 // Set the session ID.
+    res.status(200).json(ToReturn) // Reply with the json object.
+}
+
 function HandleStatusRequest(req,res) {
     let ToReturn = new Object() // Create the return json object.
-    ToReturn.error = GetErrorJson() // Storing the ErrorJson object template in the ToRetuen json object.
+    DebugPrint("Received status request.")
+    ToReturn.error = GetErrorJson() // Storing the ErrorJson object template in the ToReturn json object.
     ToReturn.status = new Object() // Create the status json object.
     ToReturn.status.errors = 0
     ToReturn.status.battery_state = 100
@@ -67,8 +94,18 @@ app.get('/position/now/:id', (req,res) => { // Endpoint to get the actual positi
     HandlePositionActualRequest(req,res) // Trigger the Position request.
 })
 
+app.get('/user/:mail/:password', (req,res) => { // Endpoint to get the actual position of a tracker based on his ID.
+    HandleUserInfoRequest(req,res) // Trigegr the user information request handler.
+})
+
 app.get('/status/', (req,res) => {
     HandleStatusRequest(req,res) // Trigger the Status handler.
+})
+
+app.post('/user/:mail/:password', (req,res) => {   
+    DebugPrint(req.params.password) 
+    //parkings.push(req.body)   
+    res.status(200).json(GetErrorJson())
 })
 
 app.listen(LISTENING_PORT, () => {  
--- API/readme.md
+++ API/readme.md
@@ -1,18 +1,46 @@
 # Documentation API REST NodeJS
 
+This API mediate between clients, MQTT Broker and the Database, the reponse format is in JSON.
 
-All response format are in json
+This file document how to call the API and how she will respond, in all response there will be a error field with the following data in it : `{"error":{"ErrorCode":0,"Message":"Nothing went wrong"}`.
+
+## Table of content
+
+[GET API Call](##get-api-call "How to make a GET API Call and what data is retuned.")
+
+[POST API Call](##post-api-call "How to make a POST API Call and what data is retuned.")
+
+[PUT API Call](##put-api-call "How to make a PUT API Call and what data is retuned.")
+
+[DELETE API Call](##delete-api-call "How to make a DELETE API Call and what data is retuned.")
+
+[Sources](#sources "The sources.")
+
+## GET API Call
 
 First API call is a GET call, this call is dedicated to request data from the server, there is 2 types of GET call :
+
 - GET without parameters :
   - Client API Request : http://127.0.0.1/status/ in this case the server return the global server status.
-  - Server API Handle : ```app.get('/status/', (req,res) => {
-    HandleStatusRequest(req,res)
-})```
-   - Example of response ```{"error":{"ErrorCode":0,"Message":"Nothing went wrong"},"status":{"errors":0,"battery_state":100,"alarm":false}}```
+  - Server API Handle : ``app.get('/status/', (req,res) => { HandleStatusRequest(req,res) })``
+  - Example of response ``{"error":{"ErrorCode":0,"Message":"Nothing went wrong"},"status":{"errors":0,"battery_state":100,"alarm":false}}``
 - GET with parameters :
   - Client API Request : http://127.0.0.1/position/history/1234 where **"1234"** is a parameter, in this case the id of the tracker.
-  - Server API Handle : ```app.get('/position/history/:id', (req,res) => {
-    HandlePositionHistoryRequest(req,res)
-})``` to get the parameter we need to : ```const id = parseInt(req.params.id)``` where **id** is the parameter name set in the ```'/position/history/:id'```
-   - Example of response ```{"error":{"ErrorCode":0,"Message":"Nothing went wrong"},"history":[{"posx":9.56454654,"posy":15.56454654,"timestamp":123456789}]}```
+  - Server API Handle : ``app.get('/position/history/:id', (req,res) => { HandlePositionHistoryRequest(req,res) })`` to get the parameter we need to : ``const id = parseInt(req.params.id)`` where **id** is the parameter name set in the ``'/position/history/:id'``
+  - Example of response ``{"error":{"ErrorCode":0,"Message":"Nothing went wrong"},"history":[{"posx":9.56454654,"posy":15.56454654,"timestamp":123456789}]}``
+
+## POST API Call
+
+TODO
+
+## PUT API Call
+
+TODO
+
+## DELETE API Call
+
+TODO
+
+# Sources
+
+###### [Welovedev Make a REST API with NodeJS and Express](https://welovedevs.com/fr/articles/node-js-api/)
