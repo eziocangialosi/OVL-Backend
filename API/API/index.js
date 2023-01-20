eb54e8664665096ed3afa8066ddc34f6b86492e6
= <=>
2023-01-20 10:01:37 +0100

    Update to use separate file for date handler.

--- API/API/index.js
+++ API/API/index.js
@@ -1,5 +1,5 @@
 const express = require('express') // Required for the REST API to work.
-
+const date = require('./date')
 const app = express() // Create the REST API
 //Middlewareapp.use(express.json())
 const LISTENING_PORT = 8080
@@ -11,28 +11,10 @@ ERROR_CODES.AccountUnauthorised = -3
 ERROR_CODES.PositionUnavailable = -4
 
 
-function GetDate() {
-    let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    let year = date_ob.getFullYear();
-    return (date + "-" + month + "-" + year);
-
-}
-
-function GetTime() {
-    let date_ob = new Date();
-    let hours = date_ob.getHours();
-    let minutes = date_ob.getMinutes();
-    let seconds = date_ob.getSeconds();
-    return (hours+":"+minutes+":"+seconds)
-
-}
-
 function DebugPrint(data) {
     if(DEBUG == true)
     {
-        console.log("["+GetDate()+"] ["+GetTime()+"] -> "+data)
+        console.log("["+date.GetDate()+"] ["+date.GetTime()+"] -> "+data)
     }
 }
 
