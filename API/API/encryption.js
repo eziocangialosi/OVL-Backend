8288f7ab1fe511f62fe7102a2180a14e728d2327
Théo MARECHAL <theo.marechal0@gmail.com>
2023-03-06 16:47:05 +0100

    Add token generation on user add

--- API/API/encryption.js
+++ API/API/encryption.js
@@ -5,6 +5,8 @@
  * const encryption = require('./encription')
 */
 var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+var base64url = require('base64url');
 function EncryptPassword(plaintextPassword, callback) {
     bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(plaintextPassword, salt, function (err, hash) {
@@ -24,4 +26,13 @@ module.exports = {
     EncryptPassword: function (pass, callback) {
         EncryptPassword(pass, callback)
     },
+    /**
+     * This function is used to get token.
+     * @example <caption>Used to get a auth token</caption>
+     * randomStringAsBase64Url(20);
+     * // Returns 'AXSGpLVjne_f7w5Xg-fWdoBwbfs' which is 27 characters length.
+     */
+    randomStringAsBase64Url: function(size) {
+        return base64url(crypto.randomBytes(size));
+    }
 }
