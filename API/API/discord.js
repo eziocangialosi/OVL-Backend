a2c4d4c51bf05b78a7eacc8c00cb48e74fbaae2d
Théo MARECHAL <theo.marechal0@gmail.com>
2023-03-23 21:41:52 +0100

    Disable Logs and update Discord

--- API/API/discord.js
+++ API/API/discord.js
@@ -7,10 +7,11 @@
 const WebhookImage = "https://cdn.discordapp.com/attachments/1055105517967638528/1080925572403822693/OVL_Logo.png" // https://cdn-icons-png.flaticon.com/512/3075/3075782.png
 const config = require('./config')
 const { Webhook, MessageBuilder  } = require('discord-webhook-node')
-const hook = new Webhook({url: config.DiscordWebhookURL,throwErrors: false,retryOnLimit: true})
+const hook = new Webhook({url: config.Discord.WebhookURL,throwErrors: false,retryOnLimit: true})
 hook.setUsername('Open Vehicle Locator | Alert System');
 function SendInfoWebhook(Head,Title,Content) {
-    const embed = new MessageBuilder()
+    if(config.Discord.Enabled) {
+        const embed = new MessageBuilder()
         .setTitle('ℹ️ '+Head+' ℹ️')
         .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
         .setURL(config.AdministrationURL)
@@ -20,45 +21,52 @@ function SendInfoWebhook(Head,Title,Content) {
         .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
         .setTimestamp();
     hook.send(embed);
+    }
 }
 
 function SendSucesssWebhook(Head,Title,Content) {
-    const embed = new MessageBuilder()
-        .setTitle('✅ '+Head+' ✅')
-        .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
-        .setURL(config.AdministrationURL)
-        .addField('**'+Title+'**', Content, true)
-        .setColor('#28c93b')
-        .setThumbnail(WebhookImage)
-        .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
-        .setTimestamp();
-    hook.send(embed);
+    if(config.Discord.Enabled) {
+        const embed = new MessageBuilder()
+            .setTitle('✅ '+Head+' ✅')
+            .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
+            .setURL(config.AdministrationURL)
+            .addField('**'+Title+'**', Content, true)
+            .setColor('#28c93b')
+            .setThumbnail(WebhookImage)
+            .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
+            .setTimestamp();
+        hook.send(embed);
+    }
 }
 
 function SendWarningWebhook(Head,Title,Content) {
-    const embed = new MessageBuilder()
-        .setTitle('⚠️ '+Head+' ⚠️')
-        .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
-        .setURL(config.AdministrationURL)
-        .addField('**'+Title+'**', Content, true)
-        .setColor('#d13e08')
-        .setThumbnail(WebhookImage)
-        .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
-        .setTimestamp();
-    hook.send(embed);
+    if(config.Discord.Enabled) {
+        const embed = new MessageBuilder()
+            .setTitle('⚠️ '+Head+' ⚠️')
+            .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
+            .setURL(config.AdministrationURL)
+            .addField('**'+Title+'**', Content, true)
+            .setColor('#d13e08')
+            .setThumbnail(WebhookImage)
+            .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
+            .setTimestamp();
+        hook.send(embed);
+    }
 }
 
 function SendErrorWebhook(Head,Title,Content) {
-    const embed = new MessageBuilder()
-        .setTitle('❌ '+Head+' ❌')
-        .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
-        .setURL(config.AdministrationURL)
-        .addField('**'+Title+'**', Content, true)
-        .setColor('#ff0000')
-        .setThumbnail(WebhookImage)
-        .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
-        .setTimestamp();
-    hook.send(embed);
+    if(config.Discord.Enabled) {
+        const embed = new MessageBuilder()
+            .setTitle('❌ '+Head+' ❌')
+            .setAuthor('Open Vehicle Locator | Alert System', WebhookImage, config.AdministrationURL)
+            .setURL(config.AdministrationURL)
+            .addField('**'+Title+'**', Content, true)
+            .setColor('#ff0000')
+            .setThumbnail(WebhookImage)
+            .setFooter('Made by Tech-User 42 with ❤️ for the open-source project Open Vehicle Locator', WebhookImage)
+            .setTimestamp();
+        hook.send(embed);
+    }
 }
 
 module.exports = {
