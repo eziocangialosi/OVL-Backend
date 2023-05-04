196fcf5d7ffd0f905fe704e3473cdfd821eb0d41
Théo MARECHAL <theo.marechal0@gmail.com>
2023-05-04 22:55:06 +0200

    Update setup scripts

--- Install/Install.sh
+++ Install/Install.sh
@@ -1,17 +1,21 @@
-echo "Starting OVL Backend installation..."
+echo "[1/7]  Starting OVL Backend installation..."
 sudo apt update
 clear
 sudo apt full-upgrade -y
 clear
-echo "Installing depedencies 1/2..."
+echo "[2/7]  Installing depedencies 1/2..."
 sudo apt install -y nodejs npm mariadb-server certbot
+clear
+echo "[3/7]  Setting up mariadb user..."
 mysql -e "UPDATE mysql.user SET Password=PASSWORD('ABigAndKomplexP@ssWord') WHERE User='root'; quit;"
+clear
+echo "[4/7]  Installing database..."
 mysql -e "CREATE DATABASE OVL; quit;"
 mysql OVL < OVL_DB_Sheme.sql
 clear
 cd ..
 cd ./API/API/
-echo "Installing depedencies 2/2..."
+echo "[5/7]  Installing depedencies 2/2..."
 sudo npm i express base64url mysql bcrypt cors mqtt nodemon discord-webhook-node expo-server-sdk express-rate-limit jsonwebtoken
 clear
 echo "Starting SSL certification server, please make sure your machine 80 port is open on your router."
@@ -21,14 +25,13 @@ echo "Launching certbot tool, please follow the install instructions here "
 sudo certbot certonly --manual
 echo "Please note down SSL certificate path, press any key to continue."
 read -n 1 -s
-# Stop the server
 sudo pkill -f "node setup_ssl.js"
 sudo nano config.js
-cd ..
-echo "Installing service [OVL_Backend.service]..."
+sleep 3
+echo "[6/7]  Installing service [OVL_Backend.service]..."
 sudo cp ../../Install/OVL_Backend.service /etc/systemd/system/ovl_backend.service
 clear
-echo "Starting service [OVL_Backend.service]..."
+echo "[1/7]  Starting service [OVL_Backend.service]..."
 sudo systemctl enable --now ovl_backend.service
 clear
 echo Installation of OVL Backend done !
