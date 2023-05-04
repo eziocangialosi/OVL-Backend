4e4aaf7a346bfd048112dddfc3058fdc93ca18e0
Théo MARECHAL <theo.marechal0@gmail.com>
2023-05-04 22:46:25 +0200

    Update setup scripts

--- API/API/setup_ssl.js
+++ API/API/setup_ssl.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.static(__dirname, { dotfiles: 'allow' } ));
 app.listen(80, () => {
-  debug.Print('Node.JS SSL Setup server now listening on port 80, please follow the cerbot instructions and make sure you can access the server outside your network.');
+  debug.Print('Node.JS SSL Setup server now listening on port 80, please follow the certbot instructions and make sure you can access the server outside your network.');
   debug.Print("For more information please visit https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca");
 });
