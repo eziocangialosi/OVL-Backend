37451e6b81a91a2b67f3665b3489128670822602
Théo MARECHAL <theo.marechal0@gmail.com>
2023-05-04 21:39:23 +0200

    Update setup scripts

--- Install/Install.sh
+++ Install/Install.sh
@@ -14,10 +14,21 @@ cd ./API/API/
 echo "Installing depedencies 2/2..."
 sudo npm i express base64url mysql bcrypt cors mqtt nodemon discord-webhook-node expo-server-sdk express-rate-limit jsonwebtoken
 clear
-echo "Installing services [OVL_Backend.service]..."
+cd ./API/
+echo "Starting SSL certification server, please make sure your machine 80 port is open on your router."
+sudo node setup_ssl.js &
+echo "Launching certbot tool, please follow the install instructions"
+sudo certbot certonly --manual
+echo "Please note down SSL certificate path, press any key to continue."
+read -n 1 -s
+# Stop the server
+sudo pkill -f "node setup_ssl.js"
+sudo nano config.js
+cd ..
+echo "Installing service [OVL_Backend.service]..."
 sudo cp ../../Install/OVL_Backend.service /etc/systemd/system/ovl_backend.service
 clear
-echo "Starting OVL_Backend service..."
+echo "Starting service [OVL_Backend.service]..."
 sudo systemctl enable --now ovl_backend.service
 clear
 echo Installation of OVL Backend done !
